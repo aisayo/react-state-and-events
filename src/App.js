ce4cbb413ee2e7c10781b123fc5e248fe19c2a98
Aysan Isayo <aisayo@icloud.com>
2021-07-14 14:14:08 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './app.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="movie-container">
+      <h1>Movie Database</h1>
+      <div>
+        <input type="text" placeholder="Search a movie..." />
+        <button>Go</button>
+      </div>
+      <div className="movie-card" id="movie-1">
+        <h2>Movie Image</h2>
+        <h3>Movie Name</h3>
+        <p>Movie Rating</p>
+        <button>Like &lt;3</button>
+      </div>
+      <div className="movie-card" id="movie-2">
+        <h2>Movie Image</h2>
+        <h3>Movie Name</h3>
+        <p>Movie Rating</p>
+        <button>Like &lt;3</button>
+      </div>
+      <div className="movie-card" id="movie-3">
+        <h2>Movie Image</h2>
+        <h3>Movie Name</h3>
+        <p>Movie Rating</p>
+        <button>Like &lt;3</button>
+      </div>
     </div>
   );
 }
